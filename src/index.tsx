03f7d063f5504fff55afb37df97a8d671d3545d1
hrj0903 <heerij93@gmail.com>
2021-11-30 01:10:18 +0900

    learned typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-
-const darkTheme = {
-  textColor: 'whitesmoke',
-  backgroundColor: '#111',
-};
-
-const lightTheme = {
-  textColor: '#111',
-  backgroundColor: 'whitesmoke',
-};
+import { lightTheme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lightTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
